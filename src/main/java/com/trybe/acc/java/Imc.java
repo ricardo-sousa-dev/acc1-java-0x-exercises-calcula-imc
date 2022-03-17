cdf38e3d782442e1af26c7e88a114c79018e8574
ricardo-sousa <rwmsousa@gmail.com>
2022-03-17 14:21:06 -0300

    formatt code

--- src/main/java/com/trybe/acc/java/Imc.java
+++ src/main/java/com/trybe/acc/java/Imc.java
@@ -8,19 +8,19 @@ public class Imc {
 	 * Método inicial.
 	 *
 	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-		System.out.println("Digite o peso:");
-		float peso = scan.nextFloat();
+    System.out.println("Digite o peso:");
+    float peso = scan.nextFloat();
 
-		System.out.println("Digite a aultura:");
-		float altura = scan.nextFloat();
+    System.out.println("Digite a aultura:");
+    float altura = scan.nextFloat();
 
-		float imc = peso / (altura * altura);
+    float imc = peso / (altura * altura);
 
-		System.out.println(imc);
-		scan.close();
+    System.out.println(imc);
+    scan.close();
 	}
 
 }
