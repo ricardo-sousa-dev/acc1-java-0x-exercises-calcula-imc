21cb68eb1e47614081e77b5e38555416558924e7
ricardo-sousa <rwmsousa@gmail.com>
2022-03-17 13:54:18 -0300

    formatt code

--- src/main/java/com/trybe/acc/java/Imc.java
+++ src/main/java/com/trybe/acc/java/Imc.java
@@ -4,9 +4,11 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Imc {
-
+  /**
+   * Método inicial.
+   *
+   */
 	public static void main(String[] args) {
-		// Ler a entrada de um número pelo console
 		Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
 		System.out.println("Digite o peso:");
